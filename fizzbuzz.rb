d788d3859e210375b919a385b25517df5adf1cb6
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-09 04:44:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if (number % 5 == 0) && (number % 3 == 0)
-    "FizzBuzz"
+  if number % 3 == 0
+    puts "Fizz"
   elsif number % 5 == 0
-    "Buzz"
-  elsif number % 3 == 0
-   "Fizz"
+    puts "Buzz"
+  elsif (number % 3 == 0) && (number % 5 == 0)
+    puts "FizzBuzz"
+  else
+    puts "nil"
   end
 end
