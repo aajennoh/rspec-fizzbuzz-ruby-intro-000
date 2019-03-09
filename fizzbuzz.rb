95b8dc4c176f4cec435b8d64a5fdbcb991eb86b6
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-09 04:45:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,5 @@
 def fizzbuzz(number)
   if number % 3 == 0
     puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  elsif (number % 3 == 0) && (number % 5 == 0)
-    puts "FizzBuzz"
-  end
+end
 end
